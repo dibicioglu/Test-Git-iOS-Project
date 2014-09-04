77dcfc9d894d05bf6af8f8891cb063660367e921
schulung <schulung@intmac01.local>
2014-09-04 16:11:49 +0200

    added a test class

---
+++ DITest.h
@@ -0,0 +1,13 @@
+//
+//  DITest.h
+//  Git Test
+//
+//  Created by schulung on 04.09.14.
+//  Copyright (c) 2014 dibicioglu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DITest : NSObject
+
+@end
---
+++ DITest.m
@@ -0,0 +1,13 @@
+//
+//  DITest.m
+//  Git Test
+//
+//  Created by schulung on 04.09.14.
+//  Copyright (c) 2014 dibicioglu. All rights reserved.
+//
+
+#import "DITest.h"
+
+@implementation DITest
+
+@end
